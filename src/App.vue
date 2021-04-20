c110498a6bd1bf156b390adf0f549edb20ba8949
Adam Lond <adam.lond@victoriaplum.com>
2021-04-20 20:30:09 +0100

    Remove Header (Temp)

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
     <div id="app">
-        <Header/>
         <PageEditor></PageEditor>
     </div>
 </template>
 
 <script>
 import PageEditor from "@/components/PageEditor";
-import Header from "@/components/Header";
 
 export default {
     name: 'App',
     components: {
-        Header,
         PageEditor
     }
 }
