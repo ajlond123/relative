d6cb8557b50269a05ae738f2034743a663d01221
Adam Lond <adam.lond@victoriaplum.com>
2021-04-20 20:26:04 +0100

    Setup base CSS files

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <div id="app">
-    <h1>Relative</h1>
-  </div>
+    <div id="app">
+        <Header/>
+        <PageEditor></PageEditor>
+    </div>
 </template>
 
 <script>
+import PageEditor from "@/components/PageEditor";
+import Header from "@/components/Header";
+
 export default {
-  name: 'App',
-  components: {}
+    name: 'App',
+    components: {
+        Header,
+        PageEditor
+    }
 }
 </script>
 
 <style>
-
+@import '~normalize.css';
+@import 'styles/index.css';
 </style>
